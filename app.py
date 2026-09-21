f5dd4abb1b78bb0f76857a20d02b6689d5950700
66050078-sketch <66050078@kmitl.ac.th>
2026-09-21 11:45:39 +0700

    Update sidebar to expanded

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
     page_title="Gold Analytics",
     page_icon="G",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
 
 # =========================================================
